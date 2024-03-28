5e0622a6fc7b812de5b63c594462d44ce251e4cf
gmfan <gaomufans@dingtalk.com>
2024-03-28 17:56:55 +0800

    feat: 移除旧链路日志

--- core/logx/types.go
+++ core/logx/types.go
@@ -3,66 +3,20 @@
 package logx
 
 import (
-	"github.com/tkgfan/got/core/strs"
 	"time"
 )
 
 const (
-	// StatusNormal 正常状态
-	StatusNormal = 0
-	// StatusErr 错误状态
-	StatusErr = 1
+	// LogCtxKey 上下文 key
+	LogCtxKey = "log_ctx"
 )
 
 type (
-	// TraceLog 链路追踪日志模型
-	TraceLog struct {
-		ID string `json:"id"`
-		// 用户 IP
-		IP string `json:"ip"`
-		// 时间戳为毫秒
-		Start int64 `json:"start"`
-		// 资源，可以是 URL 路径
+	// LogCtx 日志上下文
+	LogCtx struct {
+		Tid   string    `json:"tid"`
+		Start time.Time `json:"start"`
+		// 请求资源，可以是 URL 路径
 		Source string `json:"source"`
-		// 花费时长，此值会自动更新
-		Expensive int64  `json:"expensive"`
-		HasErr    int8   `json:"hasErr"`
-		Logs      []*Log `json:"logs"`
-	}
-
-	Log struct {
-		// 用户 IP
-		IP string `json:"ip,omitempty"`
-		// 时间戳为毫秒
-		Start int64 `json:"start"`
-		// 资源，可以是 URL 路径
-		Source string `json:"source"`
-		Info   any    `json:"info"`
-		Status int8   `json:"status"`
-		// 花费时长，此值会自动更新
-		Expensive int64 `json:"expensive"`
 	}
 )
-
-func NewTraceLog() *TraceLog {
-	return &TraceLog{
-		ID:    strs.Rand(16),
-		Start: time.Now().UnixMilli(),
-		Logs:  make([]*Log, 0),
-	}
-}
-
-func (t *TraceLog) AddLog(log *Log) {
-	// 计算 Expensive
-	curMilli := time.Now().UnixMilli()
-	log.Expensive = curMilli - log.Start
-	t.Expensive = curMilli - t.Start
-
-	t.Logs = append(t.Logs, log)
-}
-
-func NewLog() *Log {
-	return &Log{
-		Start: time.Now().UnixMilli(),
-	}
-}
