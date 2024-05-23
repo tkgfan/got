d005bf302f307becb95b29a2a698b07259345a8d
gmfan <3530553117@qq.com>
2024-05-23 11:41:43 +0800

    feat: 链路日志增加 caller

--- core/tlog/print.go
+++ core/tlog/print.go
@@ -7,16 +7,30 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"runtime"
+	"strconv"
 	"time"
 )
 
-type PrintFunc func(tid, level string, logStr string)
+type CallFrame struct {
+	Pc   uintptr
+	File string
+	Line int
+}
+
+func caller(skip int) (frame CallFrame) {
+	frame.Pc, frame.File, frame.Line, _ = runtime.Caller(skip + 1)
+	return frame
+}
+
+type PrintFunc func(tid, level string, logStr string, frame CallFrame)
 
 // Print 打印函数，可自定义
-var Print PrintFunc = func(tid, level string, logStr string) {
+var Print PrintFunc = func(tid, level string, logStr string, frame CallFrame) {
 	// 格式化时间
 	now := time.Now().Format("2006-01-02 15:04:05.000")
-	str := fmt.Sprintf("[%s] tid=%s level=%s %s\n", now, tid, level, logStr)
+	str := fmt.Sprintf("[%s] tid=%s level=%s %s caller=%s\n",
+		now, tid, level, logStr, frame.File+" "+strconv.Itoa(frame.Line))
 	if level == ErrorLevel {
 		str = "\033[31m" + str + "\033[0m"
 	} else if level == WarnLevel {
@@ -32,64 +46,82 @@ func Info(tid string, v ...any) {
 	if levelNotPass(InfoLevel) {
 		return
 	}
-	Print(tid, InfoLevel, fmt.Sprint(v...))
+	Print(tid, InfoLevel, fmt.Sprint(v...), caller(1))
 }
 
 func Infof(tid string, format string, v ...any) {
 	if levelNotPass(InfoLevel) {
 		return
 	}
-	Print(tid, InfoLevel, fmt.Sprintf(format, v...))
+	Print(tid, InfoLevel, fmt.Sprintf(format, v...), caller(1))
 }
 
 func Warn(tid string, v ...any) {
 	if levelNotPass(WarnLevel) {
 		return
 	}
-	Print(tid, WarnLevel, fmt.Sprint(v...))
+	Print(tid, WarnLevel, fmt.Sprint(v...), caller(1))
 }
 
 func Warnf(tid string, format string, v ...any) {
 	if levelNotPass(WarnLevel) {
 		return
 	}
-	Print(tid, WarnLevel, fmt.Sprintf(format, v...))
+	Print(tid, WarnLevel, fmt.Sprintf(format, v...), caller(1))
 }
 
 func Error(tid string, v ...any) {
 	if levelNotPass(ErrorLevel) {
 		return
 	}
-	Print(tid, ErrorLevel, fmt.Sprint(v...))
+	Print(tid, ErrorLevel, fmt.Sprint(v...), caller(1))
 }
 
 func Errorf(tid string, format string, v ...any) {
 	if levelNotPass(ErrorLevel) {
 		return
 	}
-	Print(tid, ErrorLevel, fmt.Sprintf(format, v...))
+	Print(tid, ErrorLevel, fmt.Sprintf(format, v...), caller(1))
 }
 
 func CtxInfo(ctx context.Context, v ...any) {
-	Info(GetTid(ctx), v...)
+	if levelNotPass(InfoLevel) {
+		return
+	}
+	Print(GetTid(ctx), InfoLevel, fmt.Sprint(v...), caller(1))
 }
 
 func CtxInfof(ctx context.Context, format string, v ...any) {
-	Infof(GetTid(ctx), format, v...)
+	if levelNotPass(InfoLevel) {
+		return
+	}
+	Print(GetTid(ctx), InfoLevel, fmt.Sprintf(format, v...), caller(1))
 }
 
 func CtxWarn(ctx context.Context, v ...any) {
-	Warn(GetTid(ctx), v...)
+	if levelNotPass(WarnLevel) {
+		return
+	}
+	Print(GetTid(ctx), WarnLevel, fmt.Sprint(v...), caller(1))
 }
 
 func CtxWarnf(ctx context.Context, format string, v ...any) {
-	Warnf(GetTid(ctx), format, v...)
+	if levelNotPass(WarnLevel) {
+		return
+	}
+	Print(GetTid(ctx), WarnLevel, fmt.Sprintf(format, v...), caller(1))
 }
 
 func CtxError(ctx context.Context, v ...any) {
-	Error(GetTid(ctx), v...)
+	if levelNotPass(ErrorLevel) {
+		return
+	}
+	Print(GetTid(ctx), ErrorLevel, fmt.Sprint(v...), caller(1))
 }
 
 func CtxErrorf(ctx context.Context, format string, v ...any) {
-	Errorf(GetTid(ctx), format, v...)
+	if levelNotPass(ErrorLevel) {
+		return
+	}
+	Print(GetTid(ctx), ErrorLevel, fmt.Sprintf(format, v...), caller(1))
 }
